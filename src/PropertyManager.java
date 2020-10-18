9e81c635d87fba02c6e1d73b42c0be1ed273b61c
jmfalzarano <48529876+jmfalzarano@users.noreply.github.com>
2020-10-18 13:01:26 -0400

    Adding missing class stubs

---
+++ src/PropertyManager.java
@@ -0,0 +1,27 @@
+
+public class PropertyManager extends User{
+	
+	private String officeAddress;
+	private Property[] properties;
+	
+	public PropertyManager(String name, String phone, String email, String password, String officeAddress) {
+		super(name, phone, email, password);
+		this.officeAddress = officeAddress;
+	}
+	
+	public String getOfficeAddress() {
+		return this.officeAddress;
+	}
+	
+	public Property[] getProperties() {
+		return this.properties;
+	}
+	
+	protected void addProperty(Property properties) {
+		//add a property
+	}
+	
+	protected void removeRating(String propertyId) {
+		//remove a property
+	}
+}
