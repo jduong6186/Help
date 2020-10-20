539dda810f9daf059d393e0ec59b879707cf5d06
jduong6186 <jduong@email.sc.edu>
2020-10-19 20:54:30 -0400

    Adding Rating Class

--- src/housingapp/Rating.java
+++ src/housingapp/Rating.java
@@ -1,15 +1,30 @@
 package housingapp;
 
+import java.util.Scanner;
+
 public class Rating {
 
     private int stars;
     private String comment;
 
     public Rating(int stars, String comment) {
-        this.stars = stars;
+        setStars(stars);
         this.comment = comment;
     }
 
+    protected void setStars(int stars) {
+    	
+    	Scanner keyboard = new Scanner(System.in);
+    	this.stars = stars;
+    	
+    	while(this.stars < 0 || this.stars > 5) {
+    		
+    		System.out.print("Please enter in a valid star amount (0-5): ");
+    		this.stars = keyboard.nextInt();
+    	}
+    	
+    }
+    
     protected int getStars() {
         return this.stars;
     }
