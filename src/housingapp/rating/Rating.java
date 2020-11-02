33eb69105bbe4a044d1aece1c00dbef2738cc6ff
jmfalzarano <48529876+jmfalzarano@users.noreply.github.com>
2020-11-01 21:55:35 -0500

    Update Rating.java

--- src/housingapp/rating/Rating.java
+++ src/housingapp/rating/Rating.java
@@ -4,14 +4,18 @@ import housingapp.RatingType;
 
 import java.security.InvalidParameterException;
 import java.util.UUID;
-
+/**
+base class of the ratings
+*/
 public abstract class Rating {
 
     private final UUID id;
     private final RatingType type;
     private int stars;
     private String comment;
-
+    /**
+    constructor
+    */
     public Rating(RatingType type, int stars, String comment) {
         this.id = UUID.randomUUID();
         this.type = type;
@@ -22,7 +26,9 @@ public abstract class Rating {
         }
         this.comment = comment;
     }
-
+    /**
+    UUID constructor
+    */
     public Rating(UUID id, RatingType type, int stars, String comment) {
         this.id = id;
         this.type = type;
@@ -33,23 +39,33 @@ public abstract class Rating {
         }
         this.comment = comment;
     }
-
+    /**
+    returns the UUID
+    */
     public UUID getId() {
         return this.id;
     }
-
+    /**
+    returns the type of ratings
+    */
     public RatingType getType() {
         return this.type;
     }
-
+    /**
+    returns the star # for the rating
+    */
     public int getStars() {
         return this.stars;
     }
-
+    /**
+    returns the comment for the rating
+    */
     public String getComment() {
         return this.comment;
     }
-
+    /**
+    sets the stars between 1-5
+    */
     public void setStars(int stars) throws InvalidParameterException {
         if (this.stars<0 || this.stars>5) {
             throw new InvalidParameterException("Parameter 'stars' has illegal value: " + stars);
